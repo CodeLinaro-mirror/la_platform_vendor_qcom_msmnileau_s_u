5949d6bf1994e98edb4080c556e8ab123a034aff
Manjunatha Madana <quic_c_mamanj@quicinc.com>
2024-09-06 14:48:58 +0530

    Change the target from msmnile_au to msmnile_au_s_u
    Change-Id: Id76ff2889409f7be650090485377a2d1d76e1b36
    Signed-off-by: Manjunatha Madana <quic_c_mamanj@quicinc.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/msmnile_au.mk
+	$(LOCAL_DIR)/msmnile_au_s_u.mk
 COMMON_LUNCH_CHOICES := \
-        msmnile_au-userdebug \
-        msmnile_au-user
+        msmnile_au_s_u-userdebug \
+        msmnile_au_s_u-user
