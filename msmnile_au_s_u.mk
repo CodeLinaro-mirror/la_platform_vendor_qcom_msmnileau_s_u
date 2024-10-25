45d7f0eaad7686a194f2816c506fa7b25f9e577d
Sindhu Kanathur <quic_skanathu@quicinc.com>
2024-10-25 16:04:39 +0530

    msmnile_au_s_u : Disable updatable apex
    Disable  updatable apex in vendor for S to U OTA.
    API level 32.
    
    Change-Id: I629cb07e5237f54347aaa848e3e996fb6753f89e
    Signed-off-by: Sindhu Kanathur <quic_skanathu@quicinc.com>

--- msmnile_au_s_u.mk
+++ msmnile_au_s_u.mk
@@ -97,9 +97,6 @@ TARGET_USES_RRO := true
 #Enable Userspace Restart
 $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 
-# Enable support for APEX updates
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Dynamic-partition enabled by default
 #BOARD_DYNAMIC_PARTITION_ENABLE := true
 #ifeq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
